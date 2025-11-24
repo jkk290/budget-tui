76fb47963ac3084b641576197b68637f2f7f2270
jkk290 <jared@kuromoto.dev>
2025-11-23 16:42:35 -1000

    Set up queries for transactions

---
+++ sql/queries/transactions.sql
@@ -0,0 +1,42 @@
+-- name: AddTransaction :one
+INSERT INTO transactions (id, amount, tx_description, tx_date, created_at, updated_at, posted, account_id, category_id)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6,
+    $7,
+    $8,
+    $9
+)
+RETURNING *;
+
+-- name: GetTransactionByID :one
+SELECT * FROM transactions
+WHERE id = $1;
+
+-- name: GetTransactionsByAccount :many
+SELECT * FROM transactions
+WHERE account_id = $1;
+
+-- name: GetTransactionsByCategory :many
+SELECT * FROM transactions
+WHERE category_id = $1;
+
+-- name: UpdateTransaction :one
+UPDATE transactions
+SET amount = $2,
+tx_description = $3,
+tx_date = $4,
+updated_at = NOW(),
+posted = $5,
+account_id = $6,
+category_id = $7
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteTransaction :exec
+DELETE FROM transactions
+WHERE id = $1;
